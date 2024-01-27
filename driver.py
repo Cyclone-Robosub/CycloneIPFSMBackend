38d5e228e5477350be186459c7ddcf8d08a75521
sam asbell <samtynanasbell@gmail.com>
2024-01-27 01:47:36 -0800

    readysetgo

---
+++ driver.py
@@ -0,0 +1,12 @@
+'''
+Authors: Sam Asbell,
+Purpose: Executable driver for the image processing backend done by Cyclone
+Done by next meeting (02/03/24): subprocess piping into embedded backend and experiment with dehazing algorithms
+determined in this meeting.
+'''
+
+import os, glob
+import numpy as np
+import cv2 as cv
+
+
